6265234bd76226b9b6640456e160f788a42fe6de
Steven Lu <tacticalazn@gmail.com>
2014-03-09 20:28:58 -0400

    Fixing install script.

--- install.sh
+++ install.sh
@@ -1,22 +1,31 @@
 #!/bin/bash
 
-echo "Creating Vagrant LAMP Stack"
-echo ''
-echo 'Hostname: '
-read hostname_input
-echo ''
-echo 'Folder: '
-read folder_input
+set -e
+
+if [ '$#' -lt 1 ]; then
+  echo "Usage: ./install.sh <project_name>"
+fi
 
 if [ -d "$folder_input" ]; then
   echo "ERROR: Directory already exists."
   exit 1
 fi
 
+echo "Cloning and creating Vagrant LAMP stack."
+
 wget https://github.com/sjlu/vagrant-lamp/archive/master.zip -O master.zip
 unzip master.zip
 rm master.zip
-mv vagrant-lamp-master $folder_input
-cd $folder_input
+mv vagrant-lamp-master $1
+cd $1
 
-sed -i '' 's/lamp\.vagrant/'${hostname_input}'/g' Vagrantfile
+sed -i '' 's/lamp\.vagrant/'$1'.vagrant/g' Vagrantfile
+
+git init
+git config -f .gitmodules --get-regexp '^submodule\..*\.path$' | 
+  while read path_key path 
+  do
+    url_key=$(echo $path_key | sed 's/\.path/.url/')
+    url=$(git config -f .gitmodules --get "$url_key")
+    git submodule add $url $path
+  done
