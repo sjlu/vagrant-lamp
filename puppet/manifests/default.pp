7cfc7730efdfc81080589d386fa5de2eedc0b2cd
Steven Lu <tacticalazn@gmail.com>
2014-03-23 19:26:21 -0400

    Breaking down into classes.

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -1,18 +1,16 @@
-class packages {
+class bootstrap {
+  group { 'puppet':
+    ensure => 'present'
+  }
+
   exec { "apt-get update":
     command => "/usr/bin/apt-get update"
   }
+}
 
+class tools {
   $packages = [
-    'apache2',
-    'php5',
-    'php-pear',
-    'mysql-server',
-    'php5-mysql',
-    'php5-suhosin',
-    'php5-json',
-    'libapache2-mod-php5',
-    'libapache2-mod-auth-mysql'
+    "vim"
   ]
 
   package { $packages:
@@ -21,4 +19,40 @@ class packages {
   }
 }
 
-include packages
+class apache {
+  package { "apache2":
+    ensure => present,
+    require => Exec["apt-get update"]
+  }
+}
+
+class php {
+  $packages = [
+    "php5",
+    "php5-cli",
+    "php5-mysql",
+    "php5-dev",
+    "php5-gd",
+    "php5-suhosin",
+    "php5-json",
+    "php5-mcrypt",
+    "libapache2-mod-php5"
+  ]
+
+  package { $packages:
+    ensure => present,
+    require => Exec["apt-get update"]
+  }
+}
+
+class mysql {
+  package { "mysql-server":
+    ensure => present,
+    require => Exec["apt-get update"]
+  }
+}
+
+include bootstrap
+include apache
+include mysql
+include php
